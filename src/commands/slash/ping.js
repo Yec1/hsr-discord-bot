b84b7b03673d3571859234d6a17f6babc5f4aa56
Cookie12123 <45198230+Cooookie16@users.noreply.github.com>
2022-11-27 02:09:11 +0800

    Update ping.js

--- src/commands/slash/ping.js
+++ src/commands/slash/ping.js
@@ -9,10 +9,12 @@ export default {
 		.setName("ping")
 		.setDescription("Replies with Pong!")
 		.setNameLocalizations({
-			"zh-TW": "延遲"
+			"zh-TW": "延遲",
+			"ja": 'ピン'
 		})
 		.setDescriptionLocalizations({
-			"zh-TW": "查看機器人延遲"
+			"zh-TW": "查看機器人延遲",
+			"ja": 'ボットのレイテンシを測定'
 		}),
 	/**
 	 *
