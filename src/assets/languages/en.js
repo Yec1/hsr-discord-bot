7b83ec4073738aac7037b5fef0104c977a285f50
Yeci <88837499+yeci226@users.noreply.github.com>
2022-11-27 11:51:25 +0800

    update

--- src/assets/languages/en.js
+++ src/assets/languages/en.js
@@ -1,5 +1,61 @@
+
 const langs = {
-	latency: "Latency"
+    //Common
+        Cmd_ComingSoon: "Coming Soon",
+        latency: "Latency",
+        none: "None",
+        unknown: "Unknown",
+        disabled: "Disabled",
+        enabled: "Enabled",
+    //emoji.js
+        emoj_iErr: "I can't find this emoji",
+        emoji_Public: "You can use this emoji without joining the server",
+    //user.js
+        user_Full_Image: "Full Image",
+    //server.js
+        //filter
+            filter_NoRole: "No Roles",
+            filter_Everyone: "Everyone",
+        //verification
+            verify_Low: "Low",
+            verify_Medium: "Medium",
+        //regions
+            br: "Brazil",
+            eu: "Europe",
+            hk: "Hongkong",
+            in: "India",
+            jp: "Japan",
+            ru: "Russia",
+            sg: "Singapore",
+            za: "South Africa",
+            au: "Australia",
+            "us-c": "Central America",
+            "us-e": "East America",
+            "us-w": "West America",
+            "us-s": "South America",
+        server_Info: "Server Info",
+        server_General: "General",
+        server_Name: "Name",
+        server_Owner: "Owner",
+        server_Region: "Region",
+        server_Unknown_Region: "Unknown Region",
+        server_Tier: "Boost Tier",
+        server_Tier_Lvl: "Level",
+        server_Filter_Lvl: "Filter Level",
+        server_Verification_Lvl: "Verification Level",
+        server_Time_Created: "Time Created",
+        server_Statistics: "Statstics",
+        server_Role_Count: "Roles",
+        server_Emoji_Count: "Emojis",
+        Regular_Emoji: "Regular",
+        Animated_Emoji: "Animated",
+        server_Member_Count: "Members",
+        server_Member_Human: "Humans",
+        server_Member_Bot: "Bots",
+        server_Channel_Count: "Channels",
+        server_Text_Channel: "Texts",
+        server_Voice_Channel: "Voices",
+        server_Boost_Count: "Boosts",
 };
 
 export default langs;
--- src/assets/languages/jp.js
+++ src/assets/languages/jp.js
@@ -1,5 +1,60 @@
 const langs = {
-	latency: "レイテンシー"
+    //Common
+        Cmd_ComingSoon: "功能未開放",
+        latency: "レイテンシー",
+        none: "無",
+        unknown: "未知",
+        disabled: "關閉",
+        enabled: "開啟",
+    //emoji.js
+        emoj_iErr: "我沒有找到這個表情符號",
+        emoji_Public: "你可以在不加入伺服器的情況下使用此表情符號",
+    //user.js
+        user_Full_Image: "完整圖片",
+    //server.js
+        //filter
+            filter_NoRole: "無身分者",
+            filter_Everyone: "所有人",
+        //verification
+            verify_Low: "低",
+            verify_Medium: "中",
+        //regions
+            br: "巴西",
+            eu: "歐洲",
+            hk: "香港",
+            in: "印度",
+            jp: "日本",
+            ru: "俄羅斯",
+            sg: "新加坡",
+            za: "南非",
+            au: "澳洲",
+            "us-c": "中美",
+            "us-e": "東美",
+            "us-w": "西美",
+            "us-s": "南美",
+        server_Info: "伺服器資訊",
+        server_General: "一般",
+        server_Name: "名稱",
+        server_Owner: "擁有者",
+        server_Region: "區域",
+        server_Unknown_Region: "未知區域",
+        server_Tier: "加成等級",
+        server_Tier_Lvl: "等級",
+        server_Filter_Lvl: "過濾等級",
+        server_Verification_Lvl: "驗證等級",
+        server_Time_Created: "創建時間",
+        server_Statistics: "狀態",
+        server_Role_Count: "身分組",
+        server_Emoji_Count: "表情符號",
+        Regular_Emoji: "靜態",
+        Animated_Emoji: "動態",
+        server_Member_Count: "成員",
+        server_Member_Human: "用戶",
+        server_Member_Bot: "機器人",
+        server_Channel_Count: "頻道",
+        server_Text_Channel: "文字",
+        server_Voice_Channel: "語音",
+        server_Boost_Count: "加成",
 };
 
 export default langs;
