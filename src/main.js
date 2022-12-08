c79fa392970dca1c57209550b0e11bce5eef8c9a
Yeci <88837499+yeci226@users.noreply.github.com>
2022-12-08 21:58:28 +0800

    discord player

--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@ import { client } from "./index.js";
 
 import { Loader } from "./core/Loader.js";
 import { Database } from "quickmongo";
+import { Player } from 'discord-player';
+import "discord-player/smoothVolume";
 
 // Global Variables
 client.config = config;
 client.db = new Database(client.config.mongoURL);
+client.player = new Player(client);
 client.loader = new Loader(client);
 await client.loader.load();
+
+
 
 client.login(client.config.token);
