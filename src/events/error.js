4b67b1276905985c1f67d77be9dcb2a2d39ad424
yeci226 <shawnyin226@gmail.com>
2022-12-11 14:16:22 +0800

    error listener

---
+++ src/events/error.js
@@ -0,0 +1,12 @@
+import { client } from "../index.js";
+client.on('error', (error) => {
+    console.log(error)
+})
+client.on('warn', (error) => {
+    console.log(error)
+})
+process.on('unhandledRejection', (error) => {
+    console.log('Unhandled promise rejection:', error)
+})
+
+process.on('uncaughtException', console.error);
