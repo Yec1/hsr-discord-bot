98ea90613434b599d115a6b15e6fdfe17493f933
yeci226 <shawnyin226@gmail.com>
2022-12-11 15:52:06 +0800

    fix

--- src/services/djsAddition.js
+++ src/services/djsAddition.js
@@ -6,10 +6,10 @@ const text = ["Hello World!", "讓Discord更方便"];
 Object.defineProperties(EmbedBuilder.prototype, {
 	setConfig: {
 		value: function (footer) {
+			var texts = "iCE - ";
+			if(footer === undefined) texts += text[Math.floor(Math.random() * text.length)]; else texts += footer
 			return this.setColor(parseInt("CFF2FF", 16)).setFooter({
-				text:
-					"iCE - " + footer ||
-					text[Math.floor(Math.random() * text.length)],
+				text: texts,
 				iconURL: client.user.displayAvatarURL()
 			});
 		},
