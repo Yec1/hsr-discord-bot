c51ac92a9a624b107f595365636b4d5a01c77d15
Yeci <shawnyin226@gmail.com>
2024-03-26 20:42:27 +0800

    fix profile location

--- src/services/profile.js
+++ src/services/profile.js
@@ -164,9 +164,12 @@ async function mainPage(playerData, interaction) {
 		ctx.stroke();
 
 		// Characters
-		const width = 1840 / playerData.characters.length;
+		const width =
+			(920 + (playerData.characters.length - 4) * 230) /
+			playerData.characters.length;
 		for (let i = 0; i < playerData.characters.length; i++) {
-			const x = 60 + i * width;
+			const x =
+				520 - (playerData.characters.length - 4) * 115 + i * width;
 			const y = 460 + 256;
 			const Width = 187;
 			const Height = 76;
