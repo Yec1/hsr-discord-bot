8d1f48f76f10eb8f9c050bb3d8212898f48c5d3b
MiniAppleTheApple <Chenxiuhua19780828@gmail.com>
2024-04-09 22:58:22 -0300

    refactor: Refactor on validateCookie

--- src/services/utilities.js
+++ src/services/utilities.js
@@ -1,4 +1,7 @@
-
+function firstMatch(text, matchExpression) {
+	const matchResult = text.match(matchExpression);
+	return matchResult == null ? matchResult : matchResult[0];
+}
 
 function validateCookie(cookie) {
 	const re = /[^; \"]{30,}/;
@@ -6,67 +9,25 @@ function validateCookie(cookie) {
 	const reTokenV2 = /[^; \"]{10,}/;
 	const reLtmidV2 = /[^; \"]{5,}/;
 
-	let match;
-	let cookie_token = (match = cookie.match(`cookie_token=${re.source}`))
-		? match[0]
-		: null;
-	let account_id = (match = cookie.match(`account_id=${reNum.source}`))
-		? match[0]
-		: null;
-	let ltoken = (match = cookie.match(`ltoken=${re.source}`))
-		? match[0]
-		: (match = cookie.match(`ltoken_v2=${reLtmidV2.source}`))
-		? match[0]
-		: null;
-	let ltuid = (match = cookie.match(`ltuid=${reNum.source}`))
-		? match[0]
-		: (match = cookie.match(`ltuid_v2=${reNum.source}`))
-		? match[0]
-		: null;
+	const matchExpressions = [
+		`ltoken=${re.source}`,
+		`ltuid=${reNum.source}`,
+		`cookie_token_v2=${reTokenV2.source}`,
+		`account_id_v2=${reNum.source}`,
+		`ltoken_v2=${reTokenV2.source}`,
+		`ltuid_v2=${reNum.source}`,
+		`ltmid_v2=${reLtmidV2.source}`,
+		`account_mid_v2=${reLtmidV2.source}`,
+		`mi18nLang=${re.source}`,
+	]
 
-	let mi18nLang = (match = cookie.match(`mi18nLang=${re.source}`))
-		? match[0]
-		: null;
+	const text = matchExpressions
+		.map(x => firstMatch(cookie, x))
+		.filter(x => x != null)
+		.join(" ");
 
-	let cookie_token_v2 = (match = cookie.match(
-		`cookie_token_v2=${reTokenV2.source}`
-	))
-		? match[0]
-		: null;
-	let account_id_v2 = (match = cookie.match(`account_id_v2=${reNum.source}`))
-		? match[0]
-		: null;
-	let ltoken_v2 = (match = cookie.match(`ltoken_v2=${reTokenV2.source}`))
-		? match[0]
-		: null;
-	let ltuid_v2 = (match = cookie.match(`ltuid_v2=${reNum.source}`))
-		? match[0]
-		: null;
-	let ltmid_v2 = (match = cookie.match(`ltmid_v2=${reLtmidV2.source}`))
-		? match[0]
-		: null;
-	let account_mid_v2 = (match = cookie.match(
-		`account_mid_v2=${reLtmidV2.source}`
-	))
-		? match[0]
-		: null;
+	return text == "" ? null : text;
 
-	let cookie_list = [];
-
-	if (cookie_token && account_id) cookie_list.push(cookie_token, account_id);
-	const tokens = [
-		ltoken,
-		ltuid,
-		cookie_token_v2,
-		account_id_v2,
-		ltoken_v2,
-		ltuid_v2,
-		ltmid_v2,
-		account_mid_v2,
-		mi18nLang
-	];
-	for (let token of tokens) if (token !== null) cookie_list.push(token);
-	return cookie_list.length === 0 ? null : cookie_list.join(" ");
 }
 
 export { validateCookie };
